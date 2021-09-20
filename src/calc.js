93a7270d2a0fa5446cf1b13f8156b424d9340b1a
abaibik <abaibik@gmail.com>
2021-09-20 17:32:15 +0200

    tests, calc from prev.version

---
+++ src/calc.js
@@ -0,0 +1,38 @@
+function checkOperand(x) {
+  if (isNaN(x)) {
+    return false;
+  }
+
+  const invalidValues = ["", null];
+  if (invalidValues.includes(x)) {
+    return false;
+  }
+
+  return true;
+}
+
+export default function calc(a, b, op) {
+  if (!["+", "-", "*", "/"].includes(op)) {
+    return NaN;
+  }
+
+  if (!checkOperand(a) || !checkOperand(b)) {
+    return NaN;
+  }
+
+  if (op === "+") {
+    return a + b;
+  }
+
+  if (op === "-") {
+    return a - b;
+  }
+
+  if (op === "*") {
+    return a * b;
+  }
+
+  if (op === "/") {
+    return a / b;
+  }
+}
