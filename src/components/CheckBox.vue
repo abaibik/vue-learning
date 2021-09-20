3cbfbed12a137de9eded5e29dcb4fb206327dc92
abaibik <abaibik@gmail.com>
2021-09-20 18:44:13 +0200

    checkbox component

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      value=""
+      id="flexCheckDefault"
+      :checked="checked"
+    />
+    <label class="form-check-label" for="flexCheckDefault">
+      On-screen keyboard
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckBox",
+  props: {
+    checked: Boolean,
+  },
+};
+</script>
