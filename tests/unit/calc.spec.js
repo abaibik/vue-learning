89ef2a9e4e8381b226280c9fe89e2d2fc6a412a2
abaibik <abaibik@gmail.com>
2021-09-20 17:32:15 +0200

    tests, calc from prev.version

---
+++ tests/unit/calc.spec.js
@@ -0,0 +1,42 @@
+import calc from "@/calc.js";
+
+describe("calc", () => {
+  const testData = [
+    [1, 1, "+", 2],
+    [1, 2, "+", 3],
+    [1, 1, "-", 0],
+    [1, 2, "-", -1],
+    [1, 1, "*", 1],
+    [1, 0, "*", 0],
+    [4, 2, "/", 2],
+    [3, 2, "/", 1.5],
+    [3, 0, "/", Infinity],
+  ];
+
+  for (const [a, b, op, expected] of testData) {
+    it(`calc(${a}, ${b}, ${op}) = ${expected}`, () => {
+      expect(calc(a, b, op)).toBe(expected);
+    });
+  }
+});
+
+describe("calc invalid argument", () => {
+  const testData = [
+    [1, 1, "qwerty"],
+    [1, 1, ""],
+    [1, "qwerty", "+"],
+    [1, "", "+"],
+    [1, null, "+"],
+    [1, undefined, "+"],
+    ["qwerty", 1, "+"],
+    ["", 1, "+"],
+    [null, 1, "+"],
+    [undefined, 1, "+"],
+  ];
+
+  for (const [a, b, op] of testData) {
+    it(`calc(${a}, ${b}, ${op}) = NaN`, () => {
+      expect(calc(a, b, op)).toBeNaN();
+    });
+  }
+});
