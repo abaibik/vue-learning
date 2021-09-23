54f1de99b2259d6c2c20ea6ec4bc7273a2bd7623
abaibik <abaibik@gmail.com>
2021-09-23 22:47:54 +0200

    inputs changes

---
+++ src/components/InputChooser.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="radios d-flex">
+    <div class="form-check m-2" v-for="input in inputs" :key="input">
+      <input
+        class="form-check-input"
+        type="radio"
+        name="flexRadioDefault"
+        :id="input"
+        :checked="input === chosenInput"
+        @change="
+          if ($event.target.checked) {
+            $emit('chosenInputChanged', input);
+          }
+        "
+      />
+      <label class="form-check-label" :for="input"> {{ input }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputChooser",
+  emits: ["chosenInputChanged"],
+  props: {
+    inputs: Array,
+    chosenInput: String,
+  },
+};
+</script>
