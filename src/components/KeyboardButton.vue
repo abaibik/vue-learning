5469b69c8e0c27acedd71bea9b44c8ec8c720db7
abaibik <abaibik@gmail.com>
2021-09-24 13:49:42 +0200

    keyboardButton component and test

---
+++ src/components/KeyboardButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="$emit('pressed', value)" class="btn btn-outline-info">
+    {{ value }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "KeyboardButton",
+  emits: ["pressed"],
+  props: {
+    value: String,
+  },
+};
+</script>
